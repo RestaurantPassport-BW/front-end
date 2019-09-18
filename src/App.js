5722f35f544e6460131ed8abb737eedd7e70aa5d
Shane Gooch <shanegooch@Shanes-iMac.local>
2019-09-18 07:40:41 -0700

    set up router/staging branch

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+import { Switch, Route } from "react-router";
+
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path={"/login"} component={Login} />
+        <Route path={"/register"} component={Register} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
