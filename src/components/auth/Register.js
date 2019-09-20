8b360baea17120ee373ce86b9d98392a15676336
Jacob Williams <JaWilliamsDev@gmail.com>
2019-09-19 22:41:35 -0400

    set up values for formik added in validation and temp added way to see info that is submitted

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -2,10 +2,18 @@ import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from 'formik';
 import * as yup from 'yup';
 import axios from 'axios';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 
-const Register= () => {
+const Register= ({errors, touched, status}) => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    if (status) {
+        setUsers([...users, status])
+    }
+}, [status])
+
 
   return (
     <>
@@ -17,11 +25,42 @@ const Register= () => {
           <button type='submit' name='submit'>Submit</button>
         </Form>
         <a href='/'>Take me back!</a>
+
+        {users.map((users, index) => (
+            <div key={index}>
+                Name: {users.name}<br />
+                Password: {users.password}<br />
+                E-mail: {users.email}<br />
+            </div>
+        ))}
     </>
   );
 }
 
 
 export default withFormik({
+  mapPropsToValues: (values) => {
+    return {
+      name: values.name || '',
+      password: values.password || '',
+      email: values.email || '',
+    }
+  },
 
+  validationSchema: yup.object().shape({
+    name: yup.string().required('Name is a required field!'),
+    password: yup.string().required('To create an account you need a password!'),
+    email: yup.string().email('E-mail must be a valid address!').required('E-mail is required'),
+  }),
+
+  handleSubmit:(values, { setStatus }) => {
+    axios.post('https://reqres.in/api/users', values)
+    .then(res => {
+      console.log(res)
+      setStatus(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
 })(Register)
