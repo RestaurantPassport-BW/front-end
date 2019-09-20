977aef20c4aab23b4d8f7bc2cc0ae797d3a9283d
Jacob Williams <JaWilliamsDev@gmail.com>
2019-09-20 19:49:20 -0400

    initial workup of grid list of restaurants

--- src/components/restaurants/RestList.js
+++ src/components/restaurants/RestList.js
@@ -1,10 +1,35 @@
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import RestCard from './RestCard'
 
-function RestList() {
+function RestList(props) {
+    const [restaurant, newRestaurant] = useState([])
+
+    useEffect(() => {
+        axios
+        .get()
+        .then(res => {
+            newRestaurant(res.data)
+        })
+        .catch(err => console.log(err))
+    })
+
+
     return (
-        <div>
-            THIS IS THE RESTAURANT LIST COMPONENT
+        <div className='restaurant-list grid-view'>
+            {restaurant.map((rest, index) => (
+                <RestCard 
+                    key={index}
+                    image={rest.image}
+                    cuisine={rest.cuisine}
+                    phone={rest.phone}
+                    address={rest.address}
+                    hoursweek={rest.week}
+                    hoursweekend={rest.weekend}
+                />
+
+            ))}
         </div>
     )
 }
