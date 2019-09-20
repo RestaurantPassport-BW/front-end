0607ad60c38003c25411ddaf675672b039178591
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-09-20 18:39:13 -0500

    Removed name and city input feilds

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -4,22 +4,18 @@ import * as yup from "yup";
 import axios from 'axios';
 
 const Login = ({ errors, touched, status }) => {
-    const [newUsers, setNewUsers] = useState([])
+    const [Users, setNewUsers] = useState([])
 
     useEffect(() => {
         if (status) {
-            setNewUsers([...newUsers, status ])
+            setNewUsers([...Users, status ])
         }
     }, [status])
 
     return (
+      <>
+      <h1>Login</h1>
         <Form>
-            {touched.name && errors.name && <p className="error">{errors.name}</p>}
-            <Field type="text" name="name" placeholder="Name:" />
-
-            {touched.city && errors.city && <p className="error">{errors.city}</p>}
-            <Field type="text" name="city" placeholder="City:" />
-
             {touched.email && errors.email && <p className="error">{errors.email}</p>}
             <Field type="email" name="email" placeholder="Email:" />
 
@@ -30,35 +26,28 @@ const Login = ({ errors, touched, status }) => {
 
           {/* displaying input data to the page to show that 
               post request is working properly */} 
-            {newUsers.map((user, index) => (
+            {Users.map((user, index) => (
               <div key={index}>
-                <div>Name: {user.name}</div>
                 <div>Email: {user.email}</div>
-                <div>City: {user.city}</div>
                 <div>Password: {user.password}</div>
               </div>
             ))}
-
         </Form>
+        </>
     )
 }
 
 export default withFormik({
 
-    mapPropsToValues: ({ name, email, password, city }) => {
+    mapPropsToValues: ({ email, password }) => {
         return {
-            name: name || "",
-            city: city || "",
             email: email || "",
             password: password || "",
         };
     },
     validationSchema: yup.object().shape({
-       name: yup.string().required("Name is Required!"),
-       city: yup.string().required("Please enter your city!"),
        email: yup.string().email("Please enter a valid email!").required("Email is required!"),
        password: yup.string().required("Password is required"),
-      
     }),
     handleSubmit: (values, {setStatus, resetForm }) => {
             
