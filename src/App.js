56ba90187b6f760365a9ffac36741698c0164633
Jacob Williams <JaWilliamsDev@gmail.com>
2019-09-19 22:18:10 -0400

    Completed form for register, setup links on app homepage to link to login/signup

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import "./App.css";
 import { Switch, Route } from "react-router";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import styled from 'styled-components'
 
+const A = styled.a`
+  padding: 20px;
+`
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route exact path='/'>
+          <h1>HomePage</h1>
+          <A href="/login">Login</A>
+          <A href="/register">Register</A>
+        </Route>
         <Route path={"/login"} component={Login} />
         <Route path={"/register"} component={Register} />
       </Switch>
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios';
+import styled from 'styled-components';
 
-export default function Register() {
-  return <div>Register</div>;
+
+const Register= () => {
+
+  return (
+    <>
+      <h2>Sign-Up!</h2>
+        <Form>
+          <Field type='text' name='name' placeholder='Name' />
+          <Field type='password' name='password' placeholder='Password'/>
+          <Field type='text' name='email' placeholder='E-mail'/>
+          <button type='submit' name='submit'>Submit</button>
+        </Form>
+        <a href='/'>Take me back!</a>
+    </>
+  );
 }
+
+
+export default withFormik({
+
+})(Register)
