3f67cf8bad11cd06d27cc84b54f09a77d6a347c1
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-09-22 18:35:04 -0500

    added the ability to choose a new file

--- src/components/header/EditUserCard.js
+++ src/components/header/EditUserCard.js
@@ -15,16 +15,14 @@ const handleShow = () => setShow(true);
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>Choose A New File</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>
+                    <input type="file"/>
+                </Modal.Body>
                 <Modal.Footer>
-                <Button variant="link" onClick={handleClose}>
-                Close
-                </Button>
-                <Button variant="link" onClick={handleClose}>
-                Save Changes
-                </Button>
+                    <Button variant="link" onClick={handleClose}>Close</Button>
+                    <Button variant="link" onClick={handleClose}>Save Changes</Button>
                 </Modal.Footer>
             </Modal>
         </div>
