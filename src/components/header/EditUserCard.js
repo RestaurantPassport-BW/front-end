fdf02f31a2d20b74177634c8a7b499ea7767beba
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-09-22 18:17:37 -0500

    changed buttons to links

--- src/components/header/EditUserCard.js
+++ src/components/header/EditUserCard.js
@@ -9,24 +9,24 @@ const handleShow = () => setShow(true);
 
     return (
         <div>
-            <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
+            <Button variant="link" onClick={handleShow}>
+            Edit Photo
+            </Button>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                <Button variant="link" onClick={handleClose}>
+                Close
+                </Button>
+                <Button variant="link" onClick={handleClose}>
+                Save Changes
+                </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
