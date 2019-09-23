4e40856bbca1b062edb17cf90f4548f15412b9ff
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-09-23 18:28:20 -0500

    login auth

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { withFormik, Form, Field } from "formik";
 import * as yup from "yup";
@@ -32,26 +31,10 @@ const Login = ({ errors, touched, status, props }) => {
 
     //         })
     // }
-=======
-import React, { useState, useEffect } from "react";
-import { withFormik, Form, Field } from "formik";
-import * as yup from "yup";
-import axios from "axios";
-
-const Login = ({ errors, touched, status }) => {
-  const [users, setNewUsers] = useState([]);
-
-  useEffect(() => {
-    if (status) {
-      setNewUsers([...users, status]);
-    }
-  }, [status]);
->>>>>>> 19cae630cc333c209cc7142f1dd07de6ac4f30c1
 
   return (
     <>
       <h1>Login</h1>
-<<<<<<< HEAD
         <Form>
             {touched.email && errors.email && <p className="error">{errors.email}</p>}
             <Field type="email" name="email" placeholder="Email:" />
@@ -86,46 +69,6 @@ export default withFormik({
         localStorage.setItem('token', JSON.stringify(data.token));
         // props.history.push('/register')
         
-=======
-      <Form>
-        {touched.email && errors.email && (
-          <p className="error">{errors.email}</p>
-        )}
-        <Field type="email" name="email" placeholder="Email:" />
-
-        {touched.password && errors.password && (
-          <p className="error">{errors.password}</p>
-        )}
-        <Field type="password" name="password" placeholder="Password:" />
-
-        <button type="submit">Submit</button>
-      </Form>
-    </>
-  );
-};
-
-export default withFormik({
-  mapPropsToValues: ({ email, password }) => {
-    return {
-      email: email || "",
-      password: password || ""
-    };
-  },
-  validationSchema: yup.object().shape({
-    email: yup
-      .string()
-      .email("Please enter a valid email!")
-      .required("Email is required!"),
-    password: yup.string().required("Password is required")
-  }),
-  handleSubmit: (values, { setStatus, resetForm }) => {
-    axios
-      .post("https://mhagner-rest-pass.herokuapp.com/api/auth/login", values)
-
-      .then(data => {
-        localStorage.setItem("token", JSON.stringify(data.token));
-
->>>>>>> 19cae630cc333c209cc7142f1dd07de6ac4f30c1
         resetForm();
       })
       .catch(err => console.log(err));
