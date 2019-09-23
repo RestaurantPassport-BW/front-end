a608a96082ff3239001ef1055872f0c12c7d57e4
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-09-22 19:46:25 -0700

    chagned names for conventions

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import "./App.css";
 import { Switch, Route } from "react-router";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
-import styled from 'styled-components'
+import styled from "styled-components";
 
-
-const A = styled.a`
+const StyledLink = styled.a`
   padding: 20px;
-`
+`;
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/'>
+        <Route exact path="/">
           <h1>HomePage</h1>
-          <A href="/login">Login</A>
-          <A href="/register">Register</A>
+          <StyledLink href="/login">Login</StyledLink>
+          <StyledLink href="/register">Register</StyledLink>
         </Route>
         <Route path={"/login"} component={Login} />
         <Route path={"/register"} component={Register} />
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,54 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as yup from "yup";
-import axios from 'axios';
+import axios from "axios";
 
 const Login = ({ errors, touched, status }) => {
-    const [Users, setNewUsers] = useState([])
+  const [users, setNewUsers] = useState([]);
 
-    useEffect(() => {
-        if (status) {
-            setNewUsers([...Users, status ])
-        }
-    }, [status])
+  useEffect(() => {
+    if (status) {
+      setNewUsers([...users, status]);
+    }
+  }, [status]);
 
-    return (
-      <>
+  return (
+    <>
       <h1>Login</h1>
-        <Form>
-            {touched.email && errors.email && <p className="error">{errors.email}</p>}
-            <Field type="email" name="email" placeholder="Email:" />
+      <Form>
+        {touched.email && errors.email && (
+          <p className="error">{errors.email}</p>
+        )}
+        <Field type="email" name="email" placeholder="Email:" />
 
-            {touched.password && errors.password && <p className="error">{errors.password}</p>}
-            <Field type="password" name="password" placeholder="Password:" />
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
+        <Field type="password" name="password" placeholder="Password:" />
 
-            <button type="submit">Submit</button>
-        </Form>
-        </>
-    )
-}
+        <button type="submit">Submit</button>
+      </Form>
+    </>
+  );
+};
 
 export default withFormik({
+  mapPropsToValues: ({ email, password }) => {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+  validationSchema: yup.object().shape({
+    email: yup
+      .string()
+      .email("Please enter a valid email!")
+      .required("Email is required!"),
+    password: yup.string().required("Password is required")
+  }),
+  handleSubmit: (values, { setStatus, resetForm }) => {
+    axios
+      .post("https://mhagner-rest-pass.herokuapp.com/api/auth/login", values)
 
-    mapPropsToValues: ({ email, password }) => {
-        return {
-            email: email || "",
-            password: password || "",
-        };
-    },
-    validationSchema: yup.object().shape({
-       email: yup.string().email("Please enter a valid email!").required("Email is required!"),
-       password: yup.string().required("Password is required"),
-    }),
-    handleSubmit: (values, {setStatus, resetForm }) => {
-            
-      axios.post("https://mhagner-rest-pass.herokuapp.com/api/auth/login", values)
-      
-      .then((data) => {
-        localStorage.setItem('token', JSON.stringify(data.token));
+      .then(data => {
+        localStorage.setItem("token", JSON.stringify(data.token));
 
         resetForm();
       })
-      .catch((err) => console.log(err));
+      .catch(err => console.log(err));
   }
-})(Login)
+})(Login);
