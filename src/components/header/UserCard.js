a26ed22fb1e4405debb64d25090afdde3e12b9fc
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-09-25 15:49:09 -0500

    chcanged logout button

--- src/components/header/UserCard.js
+++ src/components/header/UserCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import EditUserCard from "./EditUserCard";
 import { Button } from "react-bootstrap"
+import { Icon } from 'semantic-ui-react'
 
 function UserCard() {
 
@@ -11,14 +12,21 @@ function UserCard() {
     return (
         <>
         <main className="userCard">
-            <Button variant="link" color='violet' size="sm" onClick={logOut} className="logoutBtn" type="submit">| &#8592;</Button>
-            <section>
-                <img className="userCardImg" 
-                src="https://via.placeholder.com/140"
-                alt="placeholder" 
-                />
-            </section>
-            <h2 className="userName">{localStorage.getItem('user')}</h2>
+            <Button variant="link"
+             color='violet' 
+             size="sm"
+             onClick={logOut} 
+             className="logoutBtn"
+             type="submit">
+            <Icon name="cog" size="big"/>
+             </Button>
+
+            <img className="userCardImg" 
+            src="https://via.placeholder.com/140"
+            alt="placeholder" 
+            />
+        
+            <div className="userName">{localStorage.getItem('user')}</div>
             <span>45 Check-Ins | 3 Passports</span>
             {/* <EditUserCard/> */}
         </main>
