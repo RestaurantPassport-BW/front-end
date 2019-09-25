58b1283a4a75d5da2e6b24578fa751f4cf1670af
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-09-25 12:55:06 -0500

    added styles to user card

--- src/components/header/UserCard.js
+++ src/components/header/UserCard.js
@@ -10,19 +10,17 @@ function UserCard() {
   };
     return (
         <>
-        <main>
+        <main className="userCard">
             <Button variant="link" size="sm" onClick={logOut} className="logoutBtn" type="submit">| &#8592;</Button>
-            <header>
-                <img 
-                className="userCardImg"
-                src="https://via.placeholder.com/150"
+            <section>
+                <img className="userCardImg" 
+                src="https://via.placeholder.com/140"
                 alt="placeholder" 
                 />
-            </header>
-            
-            <h2>{localStorage.getItem('user')}</h2>
+            </section>
+            <p className="userName">{localStorage.getItem('user')}</p>
             <p>45 Check-Ins | 3 Passports</p>
-            <EditUserCard/>
+            {/* <EditUserCard/> */}
         </main>
         </>
     )
