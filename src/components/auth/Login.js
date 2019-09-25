fd0bddd349aa3a65851273b4efdc7d8ae8a0a525
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-09-24 19:28:16 -0500

    set user object to storage

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -10,28 +10,27 @@ const Login = ({ errors, touched }) => {
   return (
     <>
       <main>
-            <Card className="text-center">
-                <Card.Header>
-
+        <Card className="text-center">
+            <Card.Header>
                 <h1><span role='img' aria-label='breifcase'>&#128188;</span>Restaurant Passport</h1>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Title>Please log in with your email and password</Card.Title>
-                    <Card.Text>
-                        <Form className="loginForm">
-                            {touched.email && errors.email && <p className="error">{errors.email}</p>}
-                            <Field type="email" name="email" placeholder="Email:" className="input" />
-                            {touched.password && errors.password && <p className="error">{errors.password}</p>}
-                            <Field type="password" name="password" placeholder="Password:" className="input" />
-                            <Button type="submit" variant="secondary">Login</Button>
-                        </Form>
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer className="text-muted">
-                    You must register first if you haven't already in order to login <Link to="/register">Register</Link>
-                    </Card.Footer>
-            </Card>
-        </main> 
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>Please log in with your email and password</Card.Title>
+                <Card.Text>
+                    <Form className="loginForm">
+                        {touched.email && errors.email && <p className="error">{errors.email}</p>}
+                        <Field type="email" name="email" placeholder="Email:" className="input" />
+                        {touched.password && errors.password && <p className="error">{errors.password}</p>}
+                        <Field type="password" name="password" placeholder="Password:" className="input" />
+                        <Button type="submit" variant="secondary">Login</Button>
+                    </Form>
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted">
+            You must register first if you haven't already in order to login <Link to="/register">Register</Link>
+            </Card.Footer>
+        </Card>
+      </main> 
     </>
     )
 }
@@ -54,6 +53,7 @@ export default withFormik({
         .post("https://mhagner-rest-pass.herokuapp.com/api/auth/login", values)
         .then(data => {  
         localStorage.setItem('token', JSON.stringify(data.token));
+        localStorage.setItem('user', JSON.stringify('user'))
         props.history.push('/dashboard')
       })
       .catch(err => console.log(err.message));
